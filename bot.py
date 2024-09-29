3c6c6af68e6419845576dec624db921d98f0454c
🇮🇳 CØPY CØDE [Official] <164838401+MAX-COPYCODE-TG@users.noreply.github.com>
2024-09-29 13:35:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,49 +1,85 @@
-import pyromod.listen
+import logging, os, math
+import logging.config
 from pyrogram import Client, __version__
-from pyrogram.raw.all import layer 
-from Candy_Robot import LOGGER, Config
+from pyrogram.raw.all import layer
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT
+from utils import temp
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
+from datetime import datetime
+from pytz import timezone
+from pyrogram.errors import BadRequest, Unauthorized
+from plugins import web_server
+from aiohttp import web
 
-class User(Client):
+# Get logging configurations
+logging.config.fileConfig("logging.conf")
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("cinemagoer").setLevel(logging.ERROR)
+LOGGER = logging.getLogger(__name__)
+TIMEZONE = (os.environ.get("TIMEZONE", "Asia/Kolkata"))
+class Bot(Client):
+
     def __init__(self):
         super().__init__(
-            Config.USER_SESSION,
-            api_hash=Config.API_HASH,
-            api_id=Config.API_ID,
-            workers=4
+            name=SESSION,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=300,
+            plugins={"root": "plugins"},
+            sleep_threshold=10,
         )
-        self.LOGGER = LOGGER
 
     async def start(self):
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats        
         await super().start()
-        usr_bot_me = await self.get_me()
-        return (self, usr_bot_me.id)
+        await Media.ensure_indexes()
+        me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        temp.B_LINK = me.mention
+        self.username = '@' + me.username
+        curr = datetime.now(timezone(TIMEZONE))
+        date = curr.strftime('%d %B, %Y')
+        time = curr.strftime('%I:%M:%S %p')
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(LOG_STR)
+        if LOG_CHANNEL:
+            try:
+                await self.send_message(LOG_CHANNEL, text=f"<b>{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!\n\n📅 Dᴀᴛᴇ : <code>{date}</code>\n⏰ Tɪᴍᴇ : <code>{time}</code>\n🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>{TIMEZONE}</code>\n\n🉐 Vᴇʀsɪᴏɴ : <code>v{__version__} (Layer {layer})</code></b>")                      
+            except Unauthorized:
+                LOGGER.warning("Bot isn't able to send message to LOG_CHANNEL")
+            except BadRequest as e:
+                LOGGER.error(e)                         
 
-class Candy_Robot(Client):
-    USER: User = None
-    USER_ID: int = None
+    async def stop(self, *args):
+        await super().stop()
+        me = await self.get_me()
+        logging.info(f"{me.first_name} is_...  ♻️Restarting...")
 
-    def __init__(self):
-        super().__init__(
-            "bot",
-            api_hash=Config.API_HASH,
-            api_id=Config.API_ID,
-            plugins={
-                "root": "Candy_Robot"
-            },
-            workers=200,
-            bot_token=Config.BOT_TOKEN,
-            sleep_threshold=10
-        )
-        self.LOGGER = LOGGER
+    async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
 
-    async def start(self):
-        await super().start()
-        bot_details = await self.get_me()
-        self.set_parse_mode("html")
-        self.LOGGER(__name__).info(
-            f"@{bot_details.username}  started! "
-        )
-        self.USER, self.USER_ID = await User().start()
 
-app = Candy_Robot()
+        
+app = Bot()
 app.run()
